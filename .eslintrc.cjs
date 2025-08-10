e233fa495c3f6ae526f6bef7a27e680ee18dc75c
Pierre <guirguispierre@gmail.com>
2025-08-09 22:19:49 -0700

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: [
+    // You can extend Airbnb or other configs if you install them.
+  ],
+  rules: {
+    // Custom linting rules can be placed here. Since this project does not
+    // install eslint or plugins, this file serves as documentation only.
+  },
+};
